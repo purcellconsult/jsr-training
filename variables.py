1610f4231f8c0f1b9b664495dd5b1367dab9b482
purcellconsult <purcellconsult@gmail.com>
2020-03-10 21:14:46 -0700

    Add files via upload
    manipulating variables in python

---
+++ variables.py
@@ -0,0 +1,29 @@
+########################################
+# Manipulating variables in python
+# --------------------------------
+# 3/10/20
+# Create variables, use enhanced
+# math functionality.
+#
+#
+#########################################
+
+import math
+
+x = 10
+y = 20
+z = 30
+print(x)
+print(y)
+print(z)
+print('--------')
+x, y, z = 20, 30, 40
+x, y, z = z, y, x
+print(x)
+print(y)
+print(z)
+print(math.pow(5, 2))
+print(math.sqrt(159393))
+print(math.factorial(5))
+print(math.gcd(5, 200))
+
