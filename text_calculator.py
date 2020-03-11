fbe6e2db21d3ec6e3c095f3413eb9e4d7a8f1069
purcellconsult <purcellconsult@gmail.com>
2020-03-10 21:14:20 -0700

    Add files via upload
    text based calculator in python. A good exercise to continue working on.

---
+++ text_calculator.py
@@ -0,0 +1,28 @@
+#########################################
+# Text based calculator in python
+# --------------------------------
+# 03/10/20
+#
+# Read in user input as numbers and
+# add them, subtract them, multiply them,
+# etc.
+#
+#########################################
+
+print("Enter two numbers to sum ")
+add_1 = int(input("Enter in a number "))
+add_2 = int(input("Enter in a second number "))
+print(add_1,'+', add_2,'=', add_1 + add_2)
+print("{} + {} = {}".format(add_1, add_2, add_1 + add_2))
+
+print("Enter two numbers to subtract ")
+subtract_1 = int(input("Enter in a number "))
+subtract_2 = int(input("Enter in a second number "))
+print("{} - {} = {}".format(subtract_1, subtract_2, subtract_1 - subtract_2))
+
+
+# implement functionality for multiplication
+
+# implement functionality for division
+
+# implement functionality for x ^ n. Use **
