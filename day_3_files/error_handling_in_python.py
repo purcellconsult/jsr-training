d5da74fd83c48c6919dbdc0b27103186623e9b12
purcellconsult <purcellconsult@gmail.com>
2020-03-12 08:05:44 -0700

    Add files via upload
    try/except statements in python

---
+++ day_3_files/error_handling_in_python.py
@@ -0,0 +1,41 @@
+#############################
+# Error handling in python
+# ------------------------
+# try/except statements
+#
+#
+#############################
+
+# syntax errors
+# --------------
+# Also known as parsing error.
+# Below is an example of a syntax error
+# x = 'hello world!"
+
+# The other type of error are exceptions
+# Can build them using try/except statements
+# try/except statement
+
+try:
+    divide = 10 / 0
+except ZeroDivisionError:
+    print('DivisionError: Can\'t divide by zero')
+
+# try/except/finally
+
+try:
+    read_num = int(input("Read in a number "))
+except ValueError:
+    print("Not a valid number.")
+finally:
+    print("This always run regardless of what happens above")
+
+
+# try/except/else
+try:
+    read = input('Enter in some text ')
+except KeyboardInterrupt:
+    print('Keyboard Interrupt Generated! ')
+else:
+    print('Nothing funny happened! ')
+
