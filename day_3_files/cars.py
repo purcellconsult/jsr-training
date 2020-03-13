45781c7406b578a6c994dcc5f57f1c6a4453033f
purcellconsult <purcellconsult@gmail.com>
2020-03-12 17:31:53 -0700

    Add files via upload
    Modeling cars using OOP.

---
+++ day_3_files/cars.py
@@ -0,0 +1,83 @@
+#####################################
+# Learn OOP by modeling cars
+# --------------------------
+# Learn OOP in python by modeling
+# real world everyday objects.
+#
+#
+#####################################
+
+from random import choice
+
+
+class Car:
+    """
+    This class models a car in python.
+    """
+    def __init__(self, year, make, model):
+        """
+        Initializes the state of the car
+        object.
+        year: the year the car was manufactured.
+        make: the brand of the vehicle.
+        model: the name used by the manufacturer.
+        """
+        self.year = year
+        self.make = make
+        self.model = model
+        self.speed = 0
+        self.colors = ['white', 'silver', 'black', 'grey',
+                       'blue', 'red', 'brown', 'green']
+
+        self.car_color = choice(self.colors)
+
+    def get_year(self):
+        return self.year
+
+    def get_make(self):
+        return self.make
+
+    def accelerate(self, amount=5):
+        x = amount
+        if self.speed < 300 and x < 60:
+            self.speed += amount
+        else:
+            raise ValueError('Invalid Amount')
+
+    def hit_brakes(self, amount=5):
+        x = amount
+        if self.speed > 0 and x < 60:
+            self.speed -= amount
+        else:
+            raise ValueError('Invalid Amount')
+
+    def is_stopped(self):
+        if self.speed == 0:
+            return True
+        return False
+
+    def is_moving(self):
+        if self.speed > 0:
+            return True
+        else:
+            return False
+
+    def get_color(self):
+        return self.car_color
+
+    def speedometer(self):
+        return '{} mph'.format(self.speed)
+
+
+a = Car('2010', 'Honda', 'Civic Hatchback')
+print(a.speedometer())  # 0
+print(a.is_moving())
+print(a.is_stopped())
+a.accelerate()  # 5 mph
+a.accelerate()  # 10 mph
+print(a.speedometer())  # 10 mph
+print(a.year)
+print(a.get_color())  # randomly determined!
+a.hit_brakes(10)  # -10
+print(a.speedometer())  # 0
+
