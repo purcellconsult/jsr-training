4e5f4fef0d6b85c5f70d2e1b0a89c5942f5184a0
purcellconsult <purcellconsult@gmail.com>
2020-03-12 17:27:00 -0700

    Add files via upload
    debugging module python.

---
+++ day_3_files/debugging.py
@@ -0,0 +1,49 @@
+# ##############################
+# Using Pycharm's debugger
+# ------------------------
+# Set breakpoints
+# Run file in debug mode
+# Analyze results of variables in console
+################################
+
+i = 0
+while i < 10:
+    i += 1
+
+i = 50
+while i < 100:
+    i += 1
+
+j, k = 5, 10
+while j < k:
+    j += 2
+    k += 1
+
+# h
+# he
+# hel
+# hell
+# hello
+# ... hello world
+
+str1 = 'hello world'
+new_string = ''
+for x in str1:
+    new_string += x
+    print(new_string)
+
+
+for x in range(3):
+    for y in range(3):
+        pass
+    pass
+
+# Use the debugger to detect what line of code the
+# error appears. Then, fix the error.
+# ------------------------------------
+# str2 = 'hello'
+# str3 = 5
+# str4 = str2 + str3
+
+
+
